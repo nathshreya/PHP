be27d55c8fdaff8f53c50289fac60194419bba9d
nathshreya <nathshreya26@gmail.com>
2016-06-09 15:29:51 +0530

    Update pdodb.php

--- pdodb.php
+++ pdodb.php
@@ -1,1 +1,15 @@
-<html></html>
+<html>
+  <body>
+    <?php
+      $hostname;
+      $username = "abc";
+      $password = "falling";
+      
+      $db = new PDO($hostname, $username, $password,
+        array("PDO::ATTR_EMULATE_PREPARES" => false,
+        "PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION"));
+      
+      
+    ?>
+  </body>
+</html>
