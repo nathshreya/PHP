7e84f505223f405fdc69938439b8016cd35a1f8c
nathshreya <nathshreya26@gmail.com>
2016-06-09 15:46:51 +0530

    Update pdodb.php

--- pdodb.php
+++ pdodb.php
@@ -8,7 +8,7 @@
       $db = new PDO($hostname, $username, $password,
         array("PDO::ATTR_EMULATE_PREPARES" => false,
         "PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION"));
-      
+      //$db -> setAttribute("PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION");
       
     ?>
   </body>
